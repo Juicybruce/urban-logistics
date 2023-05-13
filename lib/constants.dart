71c75ca065e3262aca6766d123cfefa70ea3e6e6
leigh <Leighbeeton3@gmail.com>
2023-05-13 10:50:11 +1000

    setup

--- lib/constants.dart
+++ lib/constants.dart
@@ -2,11 +2,11 @@
 abstract class Constants {
   static const String supabaseUrl = String.fromEnvironment(
     'SUPABASE_URL',
-    defaultValue: '',
+    defaultValue: 'https://pjmmfyhifblvbqgnllfc.supabase.co',
   );
 
   static const String supabaseAnonKey = String.fromEnvironment(
     'SUPABASE_ANON_KEY',
-    defaultValue: '',
+    defaultValue: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBqbW1meWhpZmJsdmJxZ25sbGZjIiwicm9sZSI6ImFub24iLCJpYXQiOjE2ODIzODQzODMsImV4cCI6MTk5Nzk2MDM4M30.uqz2wJg5Tf6g3_b3uIad1IE9CDJx8sjhudreHCjgJWA',
   );
 }
