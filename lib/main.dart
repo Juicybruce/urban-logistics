82e59c60d21778e3ff0aa9d18a3c0e41b69d96de
leigh <Leighbeeton3@gmail.com>
2023-05-15 23:16:41 +1000

    add truck working

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
+
+
 import 'constants.dart';
 
 void main() {
@@ -114,10 +116,150 @@ class _MyHomePageState extends State<MyHomePage> {
         ),
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
+        //goes to the add truck page
+        onPressed: () => Navigator.of(context).push(
+          MaterialPageRoute(
+            builder: (BuildContext context) => const AddTruck(),
+          ),
+        ),
       ), // This trailing comma makes auto-formatting nicer for build methods.
     );
   }
 }
+
+
+// add truck page that has a form to add a truck in a diloge box with a button to add a truck to the database
+
+class AddTruck extends StatefulWidget {
+
+  const AddTruck({super.key});
+
+  @override
+  State<AddTruck> createState() => _AddTruckState();
+}
+
+class _AddTruckState extends State<AddTruck> {
+  //supabase client
+  final SupabaseClient _client = Supabase.instance.client;
+  final _formKey = GlobalKey<FormState>();
+
+  var _licensePlateController = TextEditingController();
+  var _truckTypeController = TextEditingController();
+  var _truckCapacityController = TextEditingController();
+  var _truckWeightController = TextEditingController();
+  bool _isCooling = false;
+
+
+  @override
+  Widget build(BuildContext context) {
+    //a dialogue box with a form to add a truck that has a license plate, truck type, truck capacity, truck weight, and cooling drop down
+    return AlertDialog(
+      title: const Text('Add Truck'),
+      content: Form(
+        key: _formKey,
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            TextFormField(
+              controller: _licensePlateController,
+              decoration: const InputDecoration(labelText: 'License Plate'),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a license plate';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _truckTypeController,
+              decoration: const InputDecoration(labelText: 'Truck Type'),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a truck type';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _truckCapacityController,
+              decoration: const InputDecoration(labelText: 'Truck Capacity'),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a truck capacity';
+                }
+                return null;
+              },
+            ),
+            TextFormField(
+              controller: _truckWeightController,
+              decoration: const InputDecoration(labelText: 'Truck Weight'),
+              validator: (String? value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter a truck weight';
+                }
+                return null;
+              },
+            ),
+            DropdownButton(
+              value: _isCooling,
+              onChanged: (bool? value) {
+                setState(() {
+                  _isCooling = value!;
+                });
+              },
+              items: const [
+                DropdownMenuItem(
+                  value: false,
+                  child: Text('No Cooling'),
+                ),
+                DropdownMenuItem(
+                  value: true,
+                  child: Text('Cooling'),
+                ),
+              ],
+            ),
+          ],
+        ),
+      ),
+      actions: [
+        TextButton(
+          onPressed: () => Navigator.pop(context),
+          child: const Text('Cancel'),
+        ),
+        TextButton(
+          onPressed: () async {
+            if (_formKey.currentState!.validate()) {
+              try {
+                final response = await _client.from('trucks').insert([
+                  {
+                    'license_plate': _licensePlateController.text,
+                    'truck_type': _truckTypeController.text,
+                    'space_capacity': _truckCapacityController.text,
+                    'weight_capacity': _truckWeightController.text,
+                    'cooling_capacity': _isCooling,
+                    'driver_id': '1',
+                  }
+                ]);
+              } catch (e) {
+                ScaffoldMessenger.of(context).showSnackBar(
+                  SnackBar(
+                    content: Text(e.toString()),
+                  ),
+                );
+              }
+              Navigator.pop(context);
+            }
+          },
+          child: const Text('Add'),
+        ),
+      ],
+    );
+  }
+}
+
+
+
+
+
+
+
