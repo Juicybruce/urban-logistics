0cd7960d978257b9f3733b2e6d695da9cc9578bf
clearspec11 <57431215+clearspec11@users.noreply.github.com>
2023-07-25 14:46:39 +1000

    advert form start

--- lib/merchant/newPost.dart
+++ lib/merchant/newPost.dart
@@ -1,20 +1,103 @@
+import 'dart:typed_data';
+
 import 'package:flutter/material.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
 class newPost extends StatefulWidget {
   const newPost({Key? key}) : super(key: key);
 
   @override
   State<newPost> createState() => _newPostState();
+
 }
 
+
 class _newPostState extends State<newPost> {
+  final _formKey = GlobalKey<FormState>();
+  final SupabaseClient _client = Supabase.instance.client;
+  late TextEditingController _goodsTypeController;
+  late TextEditingController _quantityController;
+  late TextEditingController _weightController;
+  late TextEditingController _sizeController;
+  late TextEditingController _pickupAddressController;
+  late TextEditingController _dropoffAddressController;
+  late TextEditingController _coolingRequiredController;
+  late TextEditingController _jobStatusController;
+  late TextEditingController _driverIdController;
+  late TextEditingController _supplierIdController;
+
+  Future<void> addPost(String jobStatus, String goodsType, int quantity,
+      int weight, int size, bool coolingRequired, String pickupAddress,
+      String dropoffAddress, int supplierId, int driverId) async {
+    final response = await _client.from('advertisements').insert([
+      {
+        'job_status': jobStatus,
+        'goods_type': goodsType,
+        'quantity': quantity,
+        'weight': weight,
+        'size': size,
+        'cooling_required': coolingRequired,
+        'pickup_address': pickupAddress,
+        'dropoff_address': dropoffAddress,
+        'supplier_id': supplierId,
+        'driver_id': driverId,
+      }
+    ]).execute();
+  }
+
+  @override
+  void initState() {
+    _goodsTypeController = TextEditingController();
+    _quantityController = TextEditingController();
+
+
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Merchant New Post"), centerTitle: true,),
-      body: Center(
-        child: Text('Merchant New Post Screen'),
+      appBar: AppBar(title: Text("Merchant New Post"),
       ),
+      body: ListView (
+          padding: const EdgeInsets.symmetric(vertical: 18, horizontal: 12),
+          children: [
+            const SizedBox(height: 18),
+            TextFormField(
+              controller: _goodsTypeController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Goods Type',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter goods type';
+                }
+                return null;
+              },
+            ),
+            const SizedBox(height: 18),
+            TextFormField(
+              controller: _quantityController,
+              decoration: const InputDecoration(
+                border: OutlineInputBorder(),
+                labelText: 'Quantity',
+              ),
+              validator: (value) {
+                if (value == null || value.isEmpty) {
+                  return 'Please enter quantity';
+                }
+                return null;
+              },
+            ),
+          ]
+    )
     );
   }
+
+  postForm() {
+
+  }
+
+
 }
--- lib/navBar.dart
+++ lib/navBar.dart
@@ -34,8 +34,8 @@ class _navBarState extends State<navBar> {
   }
 
   String userType =
-      'driver'; // TODO change this to userprefs or something or get user type from db/ Current accepted userTypes are 'merchant' and 'driver'(well anything but merchant)
-
+      //'driver'; // TODO change this to userprefs or something or get user type from db/ Current accepted userTypes are 'merchant' and 'driver'(well anything but merchant)
+      'merchant';
   late List<Widget> screens = getScreens(userType);
 
   final PageStorageBucket bucket = PageStorageBucket();
